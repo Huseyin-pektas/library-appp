da79fe47bac96e0e7173009cdc67f3ff405869e8
sefa1-sonmez <pektashuseyin72@gmail.com>
2023-04-02 19:37:00 +0300

    api ve url oluşturuldu

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import actionType from "./redux/actions/actionTypes";
 import { useSelector } from "react-redux";
 import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
+import axios from "axios";
+import api from "./api/api";
+import url from "./api/url";
 
 function App() {
 
   // const allSelector=useSelector(state=>state)
   // const bookState=useSelector(state=>state.bookState)
   // const categoriesState=useSelector(state=>state.categoriesState)
-  const { categoriesState, bookState } = useSelector(state => state)
+  // const { categoriesState, bookState } = useSelector(state => state)
 
-  console.log(bookState)
+  // console.log(bookState)
+  useEffect(()=>{
+    // axios.get("http://localhost:3004")
+    // axios ile aynı işi yapıyor.
+    api.get(url.books)
+
+
+  },[])
 
 
   return (
---
+++ src/api/url.js
@@ -0,0 +1,6 @@
+import React from "react"
+const url={
+    books:"/books",
+    categories:"/categories"
+}
+export default url
